e3f71732c0db78e6933486cf16ce1a9338074c62
Prog24 <prog24.net@gmail.com>
2017-03-05 14:35:38 +0900

    creare admin/cate and remake style.css

---
+++ admin/cate.php
@@ -0,0 +1,63 @@
+<?php
+require_once('../function/core/cate-read-all.php');
+$cates = get_cate_all();
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>管理</title>
+
+<link rel="stylesheet" href="css/reset.css">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+    <div class="main">
+        <div class="header">
+            <div class="title">A-Blog</div>
+            <div class="blog-link">ブログの確認</div>
+        </div>
+        <div class="content">
+            <div class="main-content">
+                
+                <div class="set-menu">
+                    <div class="title">管理画面</div>
+                    <div class="menu-li">
+                        <a href="index.php">ブログ設定</a>
+                        <a href="posts.php">記事管理</a>
+                        <a href="cate.php" class="checked">カテゴリ</a>
+                    </div>
+                </div>
+                
+                <div class="naiyou">
+                    
+<!--                    新規追加ボタン-->
+                    <p>カテゴリを登録することで関連した記事を探しやすくします。</p>
+                    <div class="add-cate">
+                        <form action="../function/core/cate-add.php" method="post">
+                            カテゴリ追加：
+                            <input type="text" name="cate_name">
+                            <input type="submit" value="追加" class="add">
+                        </form>
+                    </div>
+                    
+                    
+<!--                    カテゴリテーブル-->
+                    <div class="cate-table">
+                        
+                        <?php
+                            foreach($cates as $cate){
+                                echo '<div class="cate"><div class="title">'.$cate['name'].'</div><form action="../function/core/cate-delete.php" method="post"><input type="hidden" name="id" value="'.$cate['id'].'"><input type="submit" value="削除" class="delete-btn"></form></div>';
+                            }
+                        ?>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    
+</body>
+</html>
